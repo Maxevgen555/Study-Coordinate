6e17c80ef38b520cb20995cb8167ece1b64fb6bb
Maxevgen555 <maxevgen555@gmail.com>
2021-07-26 11:47:58 +0300

    Add files via upload

---
+++ CoordinateConversion/MainCoord.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoordinateConversion
+{
+    class MainCoord
+    {
+        double latitudeCoord;
+        public double LatitudeCoord
+        {
+            get
+            {
+                return latitudeCoord;
+            }
+            set
+            {
+                latitudeCoord = LatitudeCoord;
+            }
+        }
+
+        double longitudeCoord;
+        public double LongitudeCoord
+        {
+            get
+            {
+                return longitudeCoord;
+            }
+            set
+            {
+                longitudeCoord = LongitudeCoord;
+            }
+        }
+
+        public double ToMainCoord(string UserDergrees, string UserMinutes, string UserSecond)
+        {
+            double Dergrees = double.Parse(UserDergrees);
+            double Minutes = double.Parse(UserMinutes);
+            double Second = double.Parse(UserSecond);
+            double MainDegrees = Dergrees + (Minutes / 60) + (Second / 3600);
+
+            return MainDegrees;
+        }
+        public double ToMainCoord(string UserDergrees, string UserMinutes)
+        {
+            double Dergrees = double.Parse(UserDergrees);
+            double Minutes = double.Parse(UserMinutes);
+            double MainDegrees = Dergrees + (Minutes / 60);
+
+            return MainDegrees;
+        }
+        public double ToMainCoord(string UserDergrees)
+        {
+            double Dergrees = double.Parse(UserDergrees);
+            double MainDegrees = Dergrees;
+
+            return MainDegrees;
+        }
+    }
+}
---
+++ CoordinateConversion/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoordinateConversion
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите широту: ");
+           string LatitudeUserDergrees = Console.ReadLine();
+        }
+    }
+}
